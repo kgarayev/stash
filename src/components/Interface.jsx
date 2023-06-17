90cca2980266bb53c974d55297fe835d0184d09d
kgarayev <kenangarayev@gmail.com>
2023-06-17 12:17:17 +0100

    major migration to vite

---
+++ src/components/Interface.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../assets/logos/Logo4.png"; // Adjust the path if necessary
+
+const Interface = () => {
+  return (
+    <>
+      <h1>Logo</h1>
+      <img src={logo} alt="logo" />
+    </>
+  );
+};
+
+export default Interface;
