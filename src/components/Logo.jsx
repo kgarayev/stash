9b177013c2496e78afa6683e9aeb5ca7f51f8c62
kgarayev <kenangarayev@gmail.com>
2023-06-18 16:34:15 +0100

    more styling changes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 // import logo from "../assets/logos/gifs/Good_Gif.gif";
 // import logo from "../assets/logos/Logo4.png";
-import logo from "../assets/logos/Logo5.svg";
+// import logo from "../assets/logos/Logo5.svg";
+// import logo from "../assets/logos/Logo7.svg";
+import logo from "../assets/logos/Logo6.svg";
+
 import "./styles/Logo.css";
 
 const Logo = () => {
