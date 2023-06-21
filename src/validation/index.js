53ed7d5443421556257fa3187bfbe2384fb38b57
kgarayev <kenangarayev@gmail.com>
2023-06-21 22:32:24 +0100

    validator function added

---
+++ src/validation/index.js
@@ -0,0 +1,35 @@
+import Joi from "joi";
+import { register, login } from "./schemas";
+
+export const validate = async (payload, type) => {
+  let option;
+
+  switch (type) {
+    case "register":
+      // call joi
+      option = joi.object(register);
+      break;
+
+    case "login":
+      // call joi
+      option = joi.object(login);
+      break;
+
+    default:
+      break;
+  }
+
+  try {
+    const results = await option.validateAsync(payload, { aboutEarly: false });
+    return null;
+  } catch (errors) {
+    console.log(errors);
+
+    const errorsModified = {};
+    errors.details.forEach((error) => {
+      errorsModified[error.context.key] = error.message;
+    });
+
+    return errorsModified;
+  }
+};
